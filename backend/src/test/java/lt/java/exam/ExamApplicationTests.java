f3a16c1b8429ce72d7932b218689b95c59f0446b
Vitalij <vitalij.cernevic@stud.techin.lt>
2024-06-27 09:21:48 +0300

    Spring setup

---
+++ backend/src/test/java/lt/java/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.java.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
